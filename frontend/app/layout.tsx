f3693d9450396cd0b84719965160f15df28f7d73
ash <ashraja941@gmail.com>
2025-04-20 17:25:33 -0500

    updated title

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,8 +5,8 @@ import { EndpointsContext } from "./agent";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
-  title: "LangChain Gen UI",
-  description: "Generative UI application with LangChain Python",
+  title: "BrokenBasket",
+  description: "Custom meal planning app",
 };
 
 export default function RootLayout(props: { children: ReactNode }) {
